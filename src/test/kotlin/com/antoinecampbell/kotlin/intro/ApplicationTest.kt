336321b5751e7a9ccf191d5676ee152505afd65c
Antoine Campbell <antoine.s.campbell@gmail.com>
2022-05-29 17:02:07 -0400

    Updated test

--- src/test/kotlin/com/antoinecampbell/kotlin/intro/ApplicationTest.kt
+++ src/test/kotlin/com/antoinecampbell/kotlin/intro/ApplicationTest.kt
@@ -1,10 +1,16 @@
 package com.antoinecampbell.kotlin.intro
 
 import com.antoinecampbell.kotlin.intro.plugins.configureRouting
+import com.antoinecampbell.kotlin.intro.plugins.configureSerialization
+import io.ktor.client.request.accept
 import io.ktor.client.request.get
+import io.ktor.client.request.headers
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.install
 import io.ktor.server.testing.testApplication
+import io.ktor.server.websocket.WebSockets
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,11 +19,20 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
+            install(WebSockets)
             configureRouting()
+            configureSerialization()
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals(
+                """
+                {
+                  "message" : "Hello World!"
+                }
+                """.trimIndent(),
+                bodyAsText()
+            )
         }
     }
 }
